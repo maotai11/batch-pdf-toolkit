b9c31f41ab9e630dca7c7e3ed6258f668a88d678
HXG <138343525+maotai11@users.noreply.github.com>
2026-02-03 09:28:11 +0800

    Revert PDF.js worker to CDN path

--- js/pdfLoader.js
+++ js/pdfLoader.js
@@ -11,7 +11,7 @@ class PDFLoader {
     // 設定 pdf.js worker
     if (typeof pdfjsLib !== 'undefined') {
       pdfjsLib.GlobalWorkerOptions.workerSrc = 
-        '../lib/pdf.worker.min.js';
+        'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
     }
   }
 
